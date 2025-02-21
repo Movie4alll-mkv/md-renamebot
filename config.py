34beab5cd6662a36be2e8b9ecb6ec75e082c7dcb
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2025-02-21 18:16:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
     API_ID = int(os.environ.get("API_ID", 6534707))
-    API_HASH = os.environ.get("API_HASH" "4bcc61d959a9f403b2f20149cbbe627a")
+    API_HASH = os.environ.get("API_HASH" ,"4bcc61d959a9f403b2f20149cbbe627a")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5225692027:AAFwugDiuhZjwQKUDjWbXhLZlhzzFMXAHJo")
     OWNER_ID = int(os.environ.get("OWNER_ID", 1430593323))
     AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
